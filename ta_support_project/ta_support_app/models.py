fa7fae00cf984bce18e8f6cf7c28d336d789bdd7
ats <atsushi.k214@gmail.com>
2024-05-17 16:53:27 +0900

    add: databaseのモデル定義

--- ta_support_project/ta_support_app/models.py
+++ ta_support_project/ta_support_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Data(models.Model):
+    group_id = models.CharField(max_length=10)
+    text_result = models.TextField()
+    speaker_text_result = models.TextField()
+    utterance_count = models.IntegerField()
+    sentiment_value = models.FloatField()
+
+    def __str__(self):
+        return f"{self.group_id}"
