ab393c093bb91300c706fc50674e6c9f7dcfbb5e
ats <atsushi.k214@gmail.com>
2024-07-06 15:38:42 +0900

    特定のgroup_idを検索できるよう実装

--- ta_support_project/ta_support_app/views.py
+++ ta_support_project/ta_support_app/views.py
@@ -1,3 +1,4 @@
+from warnings import filters
 from rest_framework import viewsets
 from .models import Data
 from .serializers import DataSerializer
@@ -5,3 +6,5 @@ from .serializers import DataSerializer
 class DataViewSet(viewsets.ModelViewSet):
     queryset = Data.objects.all()
     serializer_class = DataSerializer
+    filter_backends = [filters.SearchFilter]
+    serach_fields = ['group_id']
