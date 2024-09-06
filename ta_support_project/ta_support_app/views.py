3b81da3ac498df9d974ed1e2e1cfa27b3f9b4467
ats <atsushi.k214@gmail.com>
2024-09-06 22:41:29 +0900

    datetimeの追加

--- ta_support_project/ta_support_app/views.py
+++ ta_support_project/ta_support_app/views.py
@@ -1,10 +1,11 @@
-from warnings import filters
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from .models import Data
 from .serializers import DataSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 class DataViewSet(viewsets.ModelViewSet):
     queryset = Data.objects.all()
     serializer_class = DataSerializer
-    filter_backends = [filters.SearchFilter]
-    serach_fields = ['group_id']
+    filter_backends = [filters.SearchFilter, DjangoFilterBackend]
+    filterset_fields = ['datetime']
+    search_fields = ['group_id']
